1c579e7d85ce7e269574b658d3d915b4fcf906c4
MR-Addict <2750417853@qq.com>
2023-02-19 20:55:08 +0800

    fix loading bug

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -23,17 +23,19 @@ export default function Card() {
       {saying && (
         <section aria-label='image' className='group'>
           <img onLoad={() => setIsLoading(false)} src={saying.pic} alt='beautiful image' />
-          <section aria-label='text' className='md:translate-y-20 md:group-hover:translate-y-0 duration-500'>
-            <span className='after:w-32 md:after:w-5 after:origin-left md:group-hover:after:scale-x-[6] after:duration-300 after:delay-200'>
-              <p>
-                {saying.month} {saying.day}
-              </p>
-            </span>
-            <span>
-              <p>{saying.zh}</p>
-              <p>{saying.en}</p>
-            </span>
-          </section>
+          {!isLoading && (
+            <section aria-label='text' className='md:translate-y-20 md:group-hover:translate-y-0 duration-500'>
+              <span className='after:w-32 md:after:w-5 after:origin-left md:group-hover:after:scale-x-[6] after:duration-300 after:delay-200'>
+                <p>
+                  {saying.month} {saying.day}
+                </p>
+              </span>
+              <span>
+                <p>{saying.zh}</p>
+                <p>{saying.en}</p>
+              </span>
+            </section>
+          )}
         </section>
       )}
     </main>
